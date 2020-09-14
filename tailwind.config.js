37a165306ef37e308da06397662139bdbbe7dc3b
Vannsl <boehner.vanessa@gmail.com>
2020-09-14 09:54:32 +0200

    chore: disable purge in tailwind since it's already configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
 module.exports = {
   theme: {},
   variants: {},
-  plugins: []
+  plugins: [],
+  purge: false
 }
