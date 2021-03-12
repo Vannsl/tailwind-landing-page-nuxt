e2b168c5a632faf4bad36c55af0d1f985c66b1a5
Adam Janiš <adam.janis@gmail.com>
2021-03-12 16:19:59 +0100

    fix typo

--- components/Features.vue
+++ components/Features.vue
@@ -19,7 +19,7 @@
       </feature-left>
       <feature-right
         headline="Status pages"
-        content="Statu pages is a great solution for all kinds of companies that want to show their actual page status or incidents to partners or visitors of your website. Just create one add your monitors via our Dashboard or simple APIs."
+        content="Status pages is a great solution for all kinds of companies that want to show their actual page status or incidents to partners or visitors of your website. Just create one add your monitors via our Dashboard or simple APIs."
       >
         <img src="~assets/img/information-tab.svg?inline" alt slot="svg" />
       </feature-right>
