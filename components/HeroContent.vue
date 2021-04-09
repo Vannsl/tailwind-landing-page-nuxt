74d58ff4df74dacc8755ef925e42b102a2d1cced
Stephen O'Connor <oconnor1986@gmail.com>
2021-04-09 19:37:46 +0100

    Update HeroContent.vue

--- components/HeroContent.vue
+++ components/HeroContent.vue
@@ -7,19 +7,12 @@
       <div
         class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left"
       >
-        <p class="uppercase tracking-loose w-full">What business are you?</p>
         <h1 class="my-4 text-5xl font-bold leading-tight">
-          Main Hero Message to sell yourself!
+          It's important to give feedback!
         </h1>
         <p class="leading-normal text-2xl mb-8">
-          Sub-hero message, not too long and not too short. Make it just right!
+          It's even more important to give feedback the right way
         </p>
-
-        <button
-          class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg"
-        >
-          Subscribe
-        </button>
       </div>
       <!--Right Col-->
       <div class="w-full md:w-3/5 py-6 text-center">
