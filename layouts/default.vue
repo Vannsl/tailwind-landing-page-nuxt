e531627dedd43d2ca9079f9ef23789e65072f0b8
Stephen O'Connor <oconnor1986@gmail.com>
2021-04-09 19:45:51 +0100

    Remove footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,18 +2,15 @@
   <div class="leading-normal tracking-normal text-white gradient">
     <the-header />
     <nuxt />
-    <the-footer />
   </div>
 </template>
 
 <script>
 import TheHeader from '@/components/TheHeader.vue'
-import TheFooter from '@/components/TheFooter.vue'
 
 export default {
   components: {
     'the-header': TheHeader,
-    'the-footer': TheFooter
   }
 }
 </script>
